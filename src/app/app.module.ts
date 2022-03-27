33a6f623a8e40e0d383218afe315a585aef26679
David BREYAULT <davidbreyault@gmail.com>
2022-03-27 14:54:55 +0200

    [US01] Création du socle technologique du projet.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+// COMPONENTS
 import { AppComponent } from './app.component';
+// ROUTING MODULES
+import { AppRoutingModule } from './app.routing.module';
+// MODULES
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // Module global de gestion de route (à importer en dernier !)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
